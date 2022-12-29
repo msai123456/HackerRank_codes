a66e3066cdf4e718eeb34de101ee820b7d36fccc
VENKATA NAGA SAI SUCHARITH.M <90145299+msai123456@users.noreply.github.com>
2022-12-29 16:01:11 +0530

    Add files via upload

---
+++ hackerRank_Codes/participants' score.py
@@ -0,0 +1,9 @@
+# participants' score sheet
+n=5
+arr=[8,2,4,5,6,6]
+lk=set(arr)
+ll=list(lk)
+print(ll[-1])
+
+list=[i for i in range(10) if i%2==0]
+print(list)
---
+++ hackerRank_Codes/textwraping.py
@@ -0,0 +1,5 @@
+import textwrap
+string="saisucharith"
+max_width=3
+out=textwrap.fill(string,max_width)
+print(out)
